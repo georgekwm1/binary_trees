28a14fa02c47fb8b2424160b49193a4f88c260e7
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-02-02 09:18:46 +0100

    minor edits

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -15,7 +15,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	count = 1;
-	
+
 	if (tree->left != NULL)
 		count += binary_tree_size(tree->left);
 	if (tree->right != NULL)
